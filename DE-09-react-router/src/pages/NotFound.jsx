92e9a3ef1346538d5c624743c69249a0d7e8a633
nurbaki <nurbakibayansal11@gmail.com>
2022-07-18 09:12:16 +0200

    nuri

---
+++ DE-09-react-router/src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const NotFound = () => {
+  return (
+    <Container className="text-center mt-4">
+      <img
+        src="https://cdn.yenicaggazetesi.com.tr/news/2022/03/150320221117469981053.jpeg"
+        alt="Not Found"
+      />
+    </Container>
+  );
+};
+
+export default NotFound;
