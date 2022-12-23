f111c1636f90c566b0d02358f9e353a225a7e100
nurbaki <nurbakibayansal11@gmail.com>
2022-12-23 15:20:37 +0100

    rretrt

--- DE-04-react-bootstrap/src/components/CourseCard.jsx
+++ DE-04-react-bootstrap/src/components/CourseCard.jsx
@@ -19,7 +19,7 @@ const CourseCard = ({ data }) => {
               key={id}
             >
               <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src={img} />
+                <Card.Img className="mt-4" variant="top" src={img} />
                 <Card.Body>
                   <Card.Title>{name}</Card.Title>
                   <Card.Text>{text}</Card.Text>
