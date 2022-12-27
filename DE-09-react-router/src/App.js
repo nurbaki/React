68571c97a66597e5d40a18c8953c7b73cc188c8f
nurbaki <nurbakibayansal11@gmail.com>
2022-12-27 14:50:38 +0100

    mlkfsdnf

--- DE-09-react-router/src/App.js
+++ DE-09-react-router/src/App.js
@@ -6,11 +6,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Teacher from "./pages/Teacher";
 import TeacherDetails from "./pages/TeacherDetails";
 import ContactForm from "./pages/ContactForm";
-
 import Home from "./pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import CardDetails from "./pages/CardDetails";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
--- DE-09-react-router/src/components/Footer.jsx
+++ DE-09-react-router/src/components/Footer.jsx
@@ -3,15 +3,12 @@ import { Navbar } from 'react-bootstrap'
 
 const Footer = () => {
   return (
-    <div>
-<Navbar  className="justify-content-center bg-dark p-4 text-light fixed-bottom">
-
- <p > Copyright {new Date().getFullYear()} </p>
-</Navbar>
-
-
-    </div>
+  <div>
+    <Navbar  className="justify-content-center bg-dark p-4 text-light fixed-bottom">
+      <p > Copyright {new Date().getFullYear()} </p>
+    </Navbar>
+  </div>
   )
-}
+};
 
 export default Footer
