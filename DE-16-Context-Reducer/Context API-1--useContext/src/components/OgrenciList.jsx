3fe4d42ff475f024a1ec8cb22ceead8e671f5914
nurbaki <nurbakibayansal11@gmail.com>
2023-01-19 12:32:18 +0100

    sasa

--- DE-16-Context-Reducer/Context API-1--useContext/src/components/OgrenciList.jsx
+++ DE-16-Context-Reducer/Context API-1--useContext/src/components/OgrenciList.jsx
@@ -5,7 +5,7 @@ const OgrenciList = () => {
   return (
     <div>
       <h1> Ögrenci Listesi</h1>
-      <h2>Color inputu degistiginde, icinde bulundugu div 'in rengini degistiren program</h2>
+      <h2>Color inputu degistiginde, icinde bulundugu div'in rengini degistiren program</h2>
       <OgrenciItem />
     </div>
   );
